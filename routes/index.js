4855c56468529bc848a86479046ea9ebffb81152
Nevan Askey <nnaskey@gmail.com>
2026-07-22 18:20:54 -0500

    resub 4

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,11 @@ const { createUser, loginUser } = require("../controllers/users");
 const userRouter = require("./users");
 const clothingItemsRouter = require("./clothingItems");
 
+router.post("/signup", createUser);
+router.post("/signin", loginUser);
 router.use("/items", clothingItemsRouter);
 router.use("/users", userRouter);
-router.post("/users/signup", createUser);
-router.post("/users/signin", loginUser);
+
 
 router.use((req, res) => {
   res.status(NOT_FOUND).send({ message: "Requested resource not found" });
